509be10e8a5471362fe33b65f7ed55cb8e7b232e
danjer <d.h.vandenakker@live.nl>
2022-02-11 20:46:23 +0100

    temp commit

--- enigma/_crack.py
+++ enigma/_crack.py
@@ -30,45 +30,39 @@ def brute_force_settings(enigma, target="AAAAAAAA"):
             return setting
 
 
-def build_loop_graph(cypher_text, crib):
-    g = nx.Graph()
-    weighted_edges = [(n1, n2, w) for w, (n1, n2) in enumerate(zip(cypher_text, crib))]
-    g.add_weighted_edges_from(weighted_edges)
-    return g
+class PlugBoardCracker:
 
+    def __init__(self, text, cypher_text):
+        self.text = text
+        self.cypher_text = cypher_text
+        self.plugboard_pairs = {l: [l2 for l2 in string.ascii_uppercase] for l in string.ascii_uppercase}
+        self.loops = self.get_loops()
 
-def list_paths(g):
-    cycles = nx.cycle_basis(g)
-    nw_cycles = []
-    for cycle in cycles:
-        shifted_cycle = deque(cycle)
-        shifted_cycle.rotate(1)
-        shifted_cycle = list(shifted_cycle)
-        nw_cycles.append([(n1, g.get_edge_data(n1, n2)['weight'], n2) for n1, n2 in zip(shifted_cycle, cycle)])
-    return nw_cycles
+    def get_loops(self):
+        g = self.build_loop_graph()
+        cycles = nx.cycle_basis(g)
+        nw_cycles = []
+        for cycle in cycles:
+            shifted_cycle = deque(cycle)
+            shifted_cycle.rotate(1)
+            shifted_cycle = list(shifted_cycle)
+            nw_cycles.append([(n1, g.get_edge_data(n1, n2)['weight'], n2) for n1, n2 in zip(shifted_cycle, cycle)])
+        return nw_cycles
 
+    def build_loop_graph(self):
+        g = nx.Graph()
+        weighted_edges = [(n1, n2, w) for w, (n1, n2) in enumerate(zip(self.cypher_text, self.text))]
+        g.add_weighted_edges_from(weighted_edges)
+        return g
 
-def check_plugboard_assumption():
-    pass
-
-
-def create_plugbaord():
-    pass
-
-
-def testje():
-    e = Enigma(plugboard_pairs="AB,OM,CD,HQ,XZ,NK,EP,WT")
-    text = "watmoetiktocheensschrijvenhierhetmoetwellanggenoegzijn".upper()
-    ct = e.encrypt(text)
-    g = build_loop_graph(ct, text)
-    print(list_paths(g))
+    def eliminate_pairs(self):
+        loop = self.loops[0]
+        for possible_
 
 
 if __name__ == "__main__":
-    ct = "IUGHLUVFAOBNEWNAGZW"
-    crib = "MARKWORTHXATTACKEDX"
-    g = build_loop_graph(ct, crib)
-    list_paths(g)
-    nx.draw_circular(g)
-    plt.show()
-    testje()
+    e = Enigma(plugboard_pairs="AB,OM,CD,HQ,XZ,NK,EP,WT")
+    text = "watmoetiktocheensschrijvenhierhetmoetwellanggenoegzijn".upper()
+    cypher_text = e.encrypt(text)
+    pbc = PlugBoardCracker(text, cypher_text)
+    pbc.eliminate_pairs()
